e6690d2dae3b05a2f67cb4cd313d74140eda9408
bllkpp <billkippe@gmail.com>
2019-11-13 10:19:58 +0100

    Added and configured index

--- src/main/java/kickstart/Application.java
+++ src/main/java/kickstart/Application.java
@@ -20,12 +20,25 @@ import org.salespointframework.SalespointSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableSalespoint
 public class Application {
 
+	private static final String LOGIN_ROUTE = "/login";
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Configuration
+	static class GymWebConfiguration implements WebMvcConfigurer {
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry){
+			registry.addViewController(LOGIN_ROUTE).setViewName("login");
+			registry.addViewController("/").setViewName("index");
+		}
 	}
 
 	@Configuration
@@ -34,7 +47,7 @@ public class Application {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests().antMatchers("/**").permitAll().and()
-					.formLogin().loginProcessingUrl("/login").and()
+					.formLogin().loginPage(LOGIN_ROUTE).loginProcessingUrl(LOGIN_ROUTE).and()
 					.logout().logoutUrl("/logout").logoutSuccessUrl("/");
 		}
 	}
