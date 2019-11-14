de4c3501a9f95e02b0be1aaef5902c2da00b839a
SoWieMarkus <wie.markus@web.de>
2019-11-14 11:12:48 +0100

    added Staff

---
+++ src/main/java/fitnessstudio/staff/Staff.java
@@ -0,0 +1,63 @@
+package fitnessstudio.staff;
+
+import org.javamoney.moneta.Money;
+import org.salespointframework.useraccount.UserAccount;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Staff {
+
+	private Money salary;
+	@Id @GeneratedValue
+	private long staffId;
+
+	private String firstName;
+	private String lastName;
+
+	@OneToOne
+	private UserAccount userAccount;
+
+	Staff() {}
+
+	Staff(UserAccount userAccount, String firstName, String lastName){
+		this.userAccount = userAccount;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public void setSalary (Money money) {
+		this.salary = money;
+	}
+
+	public Money getSalary () {
+		return this.salary;
+	}
+
+	public long getStaffId() {
+		return staffId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getUserName() {
+		return userAccount.getUsername();
+	}
+
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
+}
+
+
+
+}
---
+++ src/main/java/fitnessstudio/staff/StaffController.java
@@ -0,0 +1,8 @@
+package fitnessstudio.staff;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StaffController {
+
+}
