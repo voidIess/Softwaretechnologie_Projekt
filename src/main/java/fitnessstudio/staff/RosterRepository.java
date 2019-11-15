698ead685dc1c83d7eaa1de72280b70b0d9b4835
SoWieMarkus <wie.markus@web.de>
2019-11-15 10:15:55 +0100

    first steps

--- src/main/java/fitnessstudio/staff/RosterRepository.java
+++ src/main/java/fitnessstudio/staff/RosterRepository.java
@@ -1,5 +1,6 @@
 package fitnessstudio.staff;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.util.Streamable;
@@ -7,6 +8,10 @@ import org.springframework.data.util.Streamable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public interface RosterRepository extends CrudRepository<Roster, Long> {
-	Streamable<Roster> findByWeek(LocalDate week, Sort sort);
+public interface RosterRepository extends CrudRepository<RosterEntry, Long> {
+	Iterable<RosterEntry> findByRole(StaffRole role, Sort sort);
+
+	default Iterable<RosterEntry> findByType(StaffRole role) {
+		return findByRole(role, Sort.by("rosterEntryId").descending());
+	}
 }
