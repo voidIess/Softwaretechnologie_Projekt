735aaabc6e3926f69c469a3755317af73a955cef
voidless <juliusjedermann@gmail.com>
2019-11-16 23:48:34 +0100

    added an item cart. wont work yet

--- src/main/java/fitnessstudio/barmanagement/BarController.java
+++ src/main/java/fitnessstudio/barmanagement/BarController.java
@@ -1,10 +1,17 @@
 package fitnessstudio.barmanagement;
 
+import org.salespointframework.inventory.InventoryItem;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
+import org.salespointframework.order.Cart;
+import org.salespointframework.order.CartItem;
+import org.salespointframework.quantity.Quantity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -23,8 +30,34 @@ public class BarController {
 	String SellingCatalog(Model model){
 
 		model.addAttribute("inventory", inventory.findAll());
-
 		return("sell_catalog");
+	}
+
+	@ModelAttribute("cart")
+	Cart initializeCart() {
+		return new Cart();
+	}
+
+	@PostMapping("/addItemToCart")
+	String addItem(@RequestParam("pid") Article article, @RequestParam("number") int number, @ModelAttribute Cart cart) {
+
+		Quantity inventoryQuantity = inventory.findByProduct(article)
+			.map(InventoryItem::getQuantity)
+			.orElse(Quantity.NONE);
+
+		Quantity allreadyOrderedQuantity = cart.stream().filter(x -> x.getProduct()
+			.equals(article)).findFirst().map(CartItem::getQuantity).orElse(Quantity.NONE);
+
+		//if(!allreadyOrderedQuantity.add(Quantity.of(number)).isGreaterThan(inventoryQuantity)) {
+			cart.addOrUpdateItem(article, Quantity.of(number));
+		//}
+		return ("redirect:cart_items");
+	}
+
+	@GetMapping("/cart_items")
+	String cartItems() {
+		//model.addAttribute("cart", cart);
+		return ("cart_items");
 	}
 
 }
