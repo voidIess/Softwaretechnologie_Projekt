daf24606bd81ef58c3d9e27ad7995a862b50001f
bllkpp <billkippe@gmail.com>
2019-11-18 08:25:56 +0100

    Added ServiceLayer testclass

---
+++ src/test/java/fitnessstudio/member/services/MemberManagementUnitTests.java
@@ -0,0 +1,13 @@
+package fitnessstudio.member.services;
+
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit tests for {@link fitnessstudio.member.MemberManagement}.
+ *
+ * @author Bill Kippe
+ * https://www.baeldung.com/spring-boot-testing
+ */
+public class MemberManagementUnitTests {
+}
