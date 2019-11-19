a2b1ac05572d93a139cbd8a4f087627e1093be12
cle4rly <c.lea.rly@gmx.de>
2019-11-19 23:53:31 +0100

    changed date in pdf to german month

--- src/main/java/fitnessstudio/pdf/PdfGenerator.java
+++ src/main/java/fitnessstudio/pdf/PdfGenerator.java
@@ -3,11 +3,13 @@ package fitnessstudio.pdf;
 import com.itextpdf.layout.Document;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public interface PdfGenerator {
 
-	static Document generatePdf(Map<String, Object> map, Document d) throws IOException {
+	public static Document generatePdf(Map<String, Object> map, Document d) throws IOException {
 
 		if(map.get("type").equals("payslip")){
 			return PayslipPdfGenerator.generatePdf(map,d);
@@ -16,5 +18,15 @@ public interface PdfGenerator {
 			return null;
 		}
 
+	}
+
+	static String getGermanMonth(int num) {
+
+		if(num < 1 || num > 12) {
+			throw new IllegalArgumentException();
+		}
+
+		String[] months = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+		return months[num-1];
 	}
 }
