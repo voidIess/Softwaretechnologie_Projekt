b6cbe97950dd3adb88d2cee4f9365ff246908009
voidless <juliusjedermann@gmail.com>
2019-11-20 17:00:53 +0100

    implemendet the first two functions of the bar manager

--- src/main/java/fitnessstudio/barmanagement/BarManager.java
+++ src/main/java/fitnessstudio/barmanagement/BarManager.java
@@ -1,12 +1,16 @@
 package fitnessstudio.barmanagement;
 
 import fitnessstudio.member.Member;
-import org.aspectj.weaver.ast.Not;
+import org.salespointframework.inventory.InventoryItem;
 import org.salespointframework.inventory.MultiInventory;
+import org.salespointframework.order.Cart;
 import org.salespointframework.order.CartItem;
 import org.salespointframework.payment.PaymentMethod;
 import org.salespointframework.quantity.Quantity;
 import org.springframework.data.util.Streamable;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.money.MonetaryAmount;
@@ -14,17 +18,41 @@ import java.time.LocalDate;
 
 public class BarManager {
 
-	MultiInventory inventory;
-	BarCatalog catalog;
+	private ExpiringInventory inventory;
+	private BarCatalog catalog;
+	private Cart cart;
 
-	public Streamable<MultiInventory> getExpiredItems(){
-		throw new NotImplementedException();
+	public BarManager(ExpiringInventory inventory, BarCatalog catalog, Cart cart) {
+
+		Assert.notNull(inventory, "BarManager needs a non-null inventory");
+		Assert.notNull(catalog, "BarManager needs a non-null catalog");
+		Assert.notNull(cart, "BarManager needs a non-null cart");
+
+		this.inventory = inventory;
+		this.catalog = catalog;
+		this.cart = cart;
 	}
-	public boolean addArticleToCart(){
-		throw new NotImplementedException();
+
+
+	public Streamable<ExpiringInventoryItem> getExipredItems(){
+		return inventory.findByExpirationDateAfterOrderByExpirationDateAsc(LocalDate.now());
 	}
-	public Streamable<Article> getAllArticles(){
-		throw new NotImplementedException();
+	public boolean addArticleToCart(Article article, Quantity quantity){
+
+		Quantity inventoryQuantity = inventory.findByProduct(article).getTotalQuantity();
+
+		Quantity allreadyOrderedQuantity = cart.stream().filter(x -> x.getProduct()
+			.equals(article)).findFirst().map(CartItem::getQuantity).orElse(Quantity.NONE);
+
+		// add the desired or amount to the cart, or nothing if not enough items in stock
+		Quantity addingQuantity = allreadyOrderedQuantity.add(quantity).isGreaterThan(inventoryQuantity) ? Quantity.NONE : quantity;
+
+		cart.addOrUpdateItem(article, addingQuantity);
+
+		return !addingQuantity.equals(Quantity.NONE);
+	}
+	public Iterable<Article> getAllArticles(){
+		return catalog.findAll();
 	}
 	public Streamable<Article> getAvailableArticles(){
 		throw new NotImplementedException();
--- src/main/java/fitnessstudio/barmanagement/ExpiringInventory.java
+++ src/main/java/fitnessstudio/barmanagement/ExpiringInventory.java
@@ -9,6 +9,5 @@ import java.time.LocalDate;
 public interface ExpiringInventory extends MultiInventory<ExpiringInventoryItem> {
 	Streamable<ExpiringInventoryItem> findByExpirationDate(LocalDate localDate);
 
-	@Query("select i from #{#entityName} i where i.expirationDate < ?1")
-	Streamable<ExpiringInventoryItem> findExpiredItemsBy(LocalDate localDate);
+	Streamable<ExpiringInventoryItem> findByExpirationDateAfterOrderByExpirationDateAsc(LocalDate localDate);
 }
