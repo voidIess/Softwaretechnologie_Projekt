1f07dc8a3aab3949171bd2f0c0145ad95ba42e96
bllkpp <billkippe@gmail.com>
2019-11-26 10:43:51 +0100

    Added default member in datainitializer

--- src/main/java/fitnessstudio/member/MemberDataInitializer.java
+++ src/main/java/fitnessstudio/member/MemberDataInitializer.java
@@ -3,7 +3,6 @@ package fitnessstudio.member;
 import org.salespointframework.core.DataInitializer;
 import org.salespointframework.useraccount.Password;
 import org.salespointframework.useraccount.Role;
-import org.salespointframework.useraccount.UserAccount;
 import org.salespointframework.useraccount.UserAccountManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 @Component
-@Order(20)
+@Order(100)
 public class MemberDataInitializer implements DataInitializer {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MemberDataInitializer.class);
@@ -20,9 +19,11 @@ public class MemberDataInitializer implements DataInitializer {
 	private final UserAccountManager userAccountManager;
 	private final MemberManagement memberManagement;
 	private final MemberRepository members;
+	private final ContractManagement contractManagement;
 
 
-	MemberDataInitializer(UserAccountManager userAccountManager, MemberManagement memberManagement, MemberRepository members){
+	MemberDataInitializer(UserAccountManager userAccountManager, MemberManagement memberManagement,
+						  MemberRepository members, ContractManagement contractManagement){
 
 		Assert.notNull(userAccountManager, "UserAccountManager must not be null");
 		Assert.notNull(memberManagement, "MemberManagement must not be null");
@@ -30,6 +31,7 @@ public class MemberDataInitializer implements DataInitializer {
 		this.memberManagement = memberManagement;
 		this.userAccountManager = userAccountManager;
 		this.members = members;
+		this.contractManagement = contractManagement;
 	}
 
 	@Override
@@ -42,5 +44,11 @@ public class MemberDataInitializer implements DataInitializer {
 		userAccountManager.create("boss", Password.UnencryptedPassword.of("123"),
 			Role.of("BOSS"), Role.of("STAFF"));
 
+		if (!contractManagement.getAllContracts().isEmpty()) {
+			LOG.info("Creating default BOSS (user: 'member', pass: '123'");
+			memberManagement.createMember(new RegistrationForm("Ulli", "Bulli", "member",
+				"123", "0123456789012345678912", "0123456789",
+				contractManagement.getAllContracts().get(0).getContractId(), ""), null).authorize();
+		}
 	}
 }
