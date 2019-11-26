3f9c82bec7c15a35c6e1ac3abccf3eac5884664c
bllkpp <billkippe@gmail.com>
2019-11-26 13:30:07 +0100

    Add findAll() to StaffManagement to get all staffs

--- src/main/java/fitnessstudio/staff/StaffRepository.java
+++ src/main/java/fitnessstudio/staff/StaffRepository.java
@@ -2,6 +2,7 @@ package fitnessstudio.staff;
 
 import org.salespointframework.useraccount.UserAccount;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
 
 import java.util.Optional;
 
@@ -9,5 +10,7 @@ public interface StaffRepository extends CrudRepository<Staff, Long> {
 
 	Optional<Staff> findByUserAccount(UserAccount userAccount);
 	Optional<Staff> findById(long staffId);
+
+	Streamable<Staff> findAll();
 
 }
